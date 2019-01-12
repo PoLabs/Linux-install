432df58187fb27973fd22ed6c51c677840cd185b
Vincent Pisano <32884213+PoLabs@users.noreply.github.com>
2019-01-12 01:01:21 -0500

    more stufff

--- comet.ml
+++ comet.ml
@@ -2,3 +2,14 @@
 from comet_ml import Experiment
 
 experiment = Experiment(api_key="", project_name="", workspace="polabs")
+
+experiment.log_parameter("batch_size", batch_size) 
+
+
+params={
+    "batch_size":batch_size,
+    "epochs":epochs,
+    "num_classes":num_classes}
+
+experiment.log_parameters(params)
+
