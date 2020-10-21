165a9950acc908efa2c3512df4b6c36f6ae9b9fc
Polabs3 <vincent.pisano1@gmail.com>
2020-10-21 00:31:31 -0400

    yes

---
+++ ubuntu-post-installer.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+#brave
+sudo apt install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+
+#gitkraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+
+#filezilla
+sudo apt-get update
+sudo apt-get install filezilla -y
+
+#putty
+sudo apt-get install putty -y
+
+#R,Rstudio,Rtools
+sudo apt -y install r-base
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd64.deb
+sudo apt-get install libclang-dev -y
+sudo dpkg -i rstudio-1.3.1093-amd64.deb
+
+#VLC
+sudo apt install vlc -y
+
+#Anaconda
+wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash /tmp/Anaconda3-2020.02-Linux-x86_64.sh -y
+
