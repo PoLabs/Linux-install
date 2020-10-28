0bc0cdbe258fddf675bb58ba6d336f3f0bb9b40c
Polabs3 <vincent.pisano1@gmail.com>
2020-10-27 21:51:28 -0400

    uypdate to installer script

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -27,6 +27,11 @@ sudo dpkg -i rstudio-1.3.1093-amd64.deb
 #VLC
 sudo apt install vlc -y
 
+
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
+
+
+
 #Anaconda
 wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
 bash /tmp/Anaconda3-2020.02-Linux-x86_64.sh -y
